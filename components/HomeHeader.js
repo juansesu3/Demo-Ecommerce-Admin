7a2784d881e2e9597aacbc374494d129dbe78a9e
juansesu3 <juanseu3@hotmail.com>
2023-09-02 16:22:51 +0200

    (FEATURE(JWT Strategy, Custom login))

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -2,28 +2,29 @@ import { useSession } from "next-auth/react";
 
 const HomeHeader = () => {
   const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="text-blue-900 flex justify-between">
-      <h2 className="mt-0">
-        <div className="flex gap-2 items-center">
+      <div className="flex w-full gap-2 items-center justify-between ">
+        <div>
+          Hello, <b>{session?.user?.email}</b>
+        </div>
+        <div className="w-6 h-6">
           <img
             src={session?.user?.image}
             alt="user-image"
-            className="w-6 h-6 rounded-md sm:hidden"
+            className="w-full rounded-md sm:hidden"
           />
-          <div> Hello, <b>{session?.user?.name}</b></div>
-         
         </div>
-      </h2>
+      </div>
 
       <div className="hidden sm:block">
-        <div className="bg-gray-300 flex  gap-1 text-black rounded-lg overflow-hidden">
+        <div className=" flex  gap-1 text-black rounded-lg overflow-hidden">
           <img
             src={session?.user?.image}
             alt="user-image"
-            className="w-6 h-6"
+            className="w-12 h-12"
           />
-          <span className="px-2 ">{session?.user?.name}</span>
         </div>
       </div>
     </div>
