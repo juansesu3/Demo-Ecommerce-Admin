9722be880dfafa2bb7f3629be8079ae9c0b2d511
juansesu3 <juanseu3@hotmail.com>
2023-09-13 17:19:02 +0200

    FIX(BUG importation)

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import { mongooseConnect } from "@/lib/mongoose";
 import NextAuth, { getServerSession } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-import { User } from "@/models/User";
+import { User } from '@/models/User';
 
 const isAdminEmails = async (email) => {
   return true;
